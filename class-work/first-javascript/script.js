8e4fa2b759505da244708ad7a5bf1f604ad23887
mmchabot <mmchabot@gmail.com>
2017-10-19 15:51:34 -0400

    Completed first JS homework-rgb-color-choice

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,32 @@
+var num1 = 10 * 10;
+var num2 = 100 / 10;
+var num3 = 50 - 17;
+var num4 = 50 + 17;
+var myName = "Molly Chabot";
+
+
+document.write(num1);
+
+document.write(num2);
+
+document.write(num3);
+
+document.write(num4);
+
+document.write(myName);
+
+function hello () {
+	var greeting = 'hello!';
+
+	greeting = 'hello, world!';
+
+	alert(greeting);
+	}
+
+document.querySelector('#title').style.color = 'red';
+
+document.querySelector('#text-chunk').style.color = 'blue';
+document.querySelector('#text-chunk').style.textDecoration = 'underline';
+
+hello();
+
